fcc4f7ab32ce5ffc0f664b3ad225393535579477
Fernanda García Aguilón <fernandaspotifi@gmail.com>
2021-03-14 11:50:20 -0600

    Agregar archivos de proyecto.

---
+++ Propiedades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3
+{
+    class Propiedades
+    {
+        int NumeroCasa;
+        string DPIDueño;
+        int CuotaMantenimiento;
+
+        public int NumeroCasa1 { get => NumeroCasa; set => NumeroCasa = value; }
+        public string DPIDueño1 { get => DPIDueño; set => DPIDueño = value; }
+        public int CuotaMantenimiento1 { get => CuotaMantenimiento; set => CuotaMantenimiento = value; }
+    }
+}
